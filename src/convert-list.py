c7aafc301fa02af2fb18173a96bbca02718ca574
weuritz8u <samu.fischer@gmx.net>
2025-04-08 23:37:30 +0200

    Aktualisieren von convert-list.py

--- src/convert-list.py
+++ src/convert-list.py
@@ -14,7 +14,7 @@ def read_list(path):
         sys.exit(1)
 
     try:
-        with open(os.path.join(path, output_file), 'wt', encoding='UTF-8') as o:
+        with open(os.path.join(path, output_file), 'wt', encoding = 'UTF-8') as o:
             o.write('const list = `')
             o.writelines(lines)
             o.write('`;')
@@ -23,11 +23,25 @@ def read_list(path):
         print('Error: No write Access')
         sys.exit(1)
 
+    try:
+        with open(os.path.join(path, output_file2), 'wt', encoding = 'UTF-8') as h:
+            h.write('<ul><li>')
+            for e in lines:
+                lines = lines.split(",")[0]
+                h.write(lines[e])
+                h.write('</li>')
+            h.write('</ul>')
+
+    expect:
+        print('Error: No write Access')
+        sys.exit(1)
+
 # run on execution
 if __name__ == '__main__':
 
     input_file = 'project-list.csv'
     output_file = 'project-list_js_var.txt'
+    output_file2 = 'project-list_md_index.txt'
 
     path = os.path.dirname(os.path.abspath(__file__))
 
