a81bf8f6dde36e6809d985fd45c210d7ad173b48
weuritz8u <128976697+ShadowDara@users.noreply.github.com>
2025-04-14 21:36:22 +0200

    remove useless stuff

--- src/sort.js
+++ src/sort.js
@@ -4,10 +4,7 @@
 async function load_data() {
     try {
         const response = await fetch("https://repo-database-creator.vercel.app/api/repos_raw");
-        console.log("Antwort Status:", response.status);
-        console.log("Antwort Header:", response.headers);
         const html = await response.text();
-        console.log("Antwort Inhalt:", html);
         return html;
     } catch (err) {
         console.error("Fehler:", err);
@@ -15,41 +12,18 @@ async function load_data() {
     }
 }
 
-// Liest eine lokale CSV-Datei ein
-document.getElementById('csvFileInput').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    if (!file) return;
-
-    const reader = new FileReader();
-    reader.onload = function(e) {
-        createTableFromCSV(e.target.result);
-    };
-
-    reader.readAsText(file);
-});
-
-// Erstellt eine HTML-Tabelle aus CSV-Text
 function createTableFromCSV(csvText) {
-    const rows = csvText.trim().split("\n").map(row => {
-        // Felder entschachteln ("escaped") und Kommas in Anführungszeichen beachten
-        return row.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g)?.map(cell => {
-            // Entferne umschließende Anführungszeichen und ersetze doppelte Anführungszeichen
-            return cell.replace(/^"(.*)"$/, '$1').replace(/""/g, '"').trim();
-        }) ?? [];
-    });
-
+    const parsed = Papa.parse(csvText.trim(), { header: false });
     const table = document.getElementById("csvTable");
     table.innerHTML = "";
 
-    rows.forEach((row, rowIndex) => {
+    parsed.data.forEach((row, rowIndex) => {
         const tr = document.createElement("tr");
-
         row.forEach(cell => {
             const cellElement = rowIndex === 0 ? document.createElement("th") : document.createElement("td");
             cellElement.textContent = cell;
             tr.appendChild(cellElement);
         });
-
         table.appendChild(tr);
     });
 }
@@ -59,6 +33,13 @@ async function display_list() {
     const list = await load_data();
     if (!list) return;
 
-    document.getElementById('table_paste').innerHTML = '<div class="table_container"><table id="csvTable"></table></div>';
+    document.getElementById('data_paste').innerHTML = '<div class="table_container"><table id="csvTable"></table></div>';
     createTableFromCSV(list);
 }
+
+async function  display_list_pic() {
+    const list = await load_data();
+    if (!list) return;
+    document.getElementById('data_paste').innerHTML = '<div></div>'
+}
+
